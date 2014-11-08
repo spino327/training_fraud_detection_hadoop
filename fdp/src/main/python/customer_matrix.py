92cc321f92a42ed8b9aceb2f655524282c639aec
pinogal <pinogal@9f61e818-6d77-41a5-bb59-0b9c5eafbe74>
2014-11-08 00:04:55 +0000

    Fix bug in customer transition matrix generation. Now the rows sum close to 1.0
    git-svn-id: https://svn.capsl.udel.edu/svn/eleg652-14f/project-by-mentors@11 9f61e818-6d77-41a5-bb59-0b9c5eafbe74

--- fdp/src/main/python/customer_matrix.py
+++ fdp/src/main/python/customer_matrix.py
@@ -1,9 +1,43 @@
 #/usr/bin/env python
 # -*- coding: utf -*-
 
+#	
+# Copyright (C) 2014 Computer Architecture and Parallel Systems Laboratory (CAPSL)	
+#
+# Original author: Tu Hao	
+# E-Mail: tuhao@udel.edu
+# 
+# Some improvements
+# author: Sergio Pino
+# E-mail: sergiop@udel.edu
+# 
+# License
+# 	
+# Redistribution of this code is allowed only after an explicit permission is
+# given by the original author or CAPSL and this license should be included in
+# all files, either existing or new ones. Modifying the code is allowed, but
+# the original author and/or CAPSL must be notified about these modifications.
+# The original author and/or CAPSL is also allowed to use these modifications
+# and publicly report results that include them. Appropriate acknowledgments
+# to everyone who made the modifications will be added in this case.
+#
+# Warranty	
+#
+# THIS CODE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTY OF ANY KIND,
+# EITHER EXPRESSED OR IMPLIED, INCLUDING, WITHOUT LIMITATION, WARRANTIES THAT
+# THE COVERED CODE IS FREE OF DEFECTS, MERCHANTABLE, FIT FOR A PARTICULAR
+# PURPOSE OR NON-INFRINGING. THE ENTIRE RISK AS TO THE QUALITY AND PERFORMANCE
+# OF THE COVERED CODE IS WITH YOU. SHOULD ANY COVERED CODE PROVE DEFECTIVE IN
+# ANY RESPECT, YOU (NOT THE INITIAL DEVELOPER OR ANY OTHER CONTRIBUTOR) ASSUME
+# THE COST OF ANY NECESSARY SERVICING, REPAIR OR CORRECTION. THIS DISCLAIMER
+# OF WARRANTY CONSTITUTES AN ESSENTIAL PART OF THIS LICENSE. NO USE OF ANY
+# COVERED CODE IS AUTHORIZED HEREUNDER EXCEPT UNDER THIS DISCLAIMER.
+#
+
 import string
 import random
 import numpy as np
+import sys
 
 STATE_NUM = 18
 
@@ -30,10 +64,24 @@ def customer_matrix_generator():
 	# add 1 to every item to ensure no item equal zero
 	mat = mat + np.ones((STATE_NUM, STATE_NUM))
 	# get sum of each row
-	row_sum = np.sum(mat, axis=0, dtype=float)
+	row_sum = [0.0]*STATE_NUM
+	for row in range(STATE_NUM):
+		s_row = sum(mat[row])
+		row_sum[row] = s_row
+	
 	# each item in a row divide sum
-	prob_mat = np.divide(mat, row_sum)
-	return prob_mat
+	for row in range(STATE_NUM):
+		for col in range(STATE_NUM):
+			mat[row][col] = mat[row][col] / row_sum[row]
+	
+	# checks
+	for row in range(STATE_NUM):
+		s_row = sum(mat[row])
+		if abs(s_row - 1.0) > 0.05:
+			print "ERROR: For row " + str(row) + " the sum was " + str(s_row)
+			sys.exit(-1)
+	
+	return mat
 
 if __name__ == "__main__":
 	PREFIX = 'customers/'
